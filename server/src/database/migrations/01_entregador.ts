c7a4a8a751ad90749b732f6d08ddb803476b782f
Willyan Marques <willyanmvs@gmail.com>
2020-06-24 11:51:40 -0300

    Ajustes na estrutura da tabela entretador

--- server/src/database/migrations/01_entregador.ts
+++ server/src/database/migrations/01_entregador.ts
@@ -4,7 +4,7 @@ export async function up(knex: Knex) {
     knex.schema.hasTable('entregador').then(exists => {
         if (!exists) {
             return knex.schema.createTable('entregador', table => {
-                table.increments('id').primary();
+                table.increments('id_entregador').primary();
                 table.string('nome').notNullable();
                 table.string('sobrenome').notNullable();
                 table.string('email');
@@ -14,6 +14,7 @@ export async function up(knex: Knex) {
                 table.string('bairro').notNullable();
                 table.string('cidade').notNullable();
                 table.string('uf', 2).notNullable();
+                table.integer('status', 1).notNullable();
                 table.timestamp('dataHora').defaultTo(knex.fn.now());
             });
         }
