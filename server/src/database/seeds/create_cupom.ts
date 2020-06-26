88044104d4e65effaa08abf9169db2e0f059fcea
Willyan Marques <willyanmvs@gmail.com>
2020-06-26 02:01:04 -0300

    ajuste no seed de cupom

--- server/src/database/seeds/create_cupom.ts
+++ server/src/database/seeds/create_cupom.ts
@@ -8,7 +8,8 @@ import Knex from 'knex';
 
 export async function seed(knex: Knex) {
     await knex('cupom').insert([
-        { codigo: 'SUPER10', descricao: 'Ganhe 10% de desconto!', valor: 10, validade: '2020-12-30', ativo: 1 },
-        { codigo: 'SUPER20', descricao: 'Ganhe 20% de desconto!', valor: 10, validade: '2020-12-30', ativo: 1 }
+        { codigo: 'SUPER10', descricao: 'Ganhe 10% de desconto!', valor: 10, is_percentual: 1, validade: '2020-12-30', ativo: 1 },
+        { codigo: 'SUPER20', descricao: 'Ganhe 20% de desconto!', valor: 20, is_percentual: 0, validade: '2020-11-30', ativo: 0 },
+        { codigo: 'SUPER30', descricao: 'Ganhe 30% de desconto!', valor: 30, is_percentual: 1, validade: '2020-10-30', ativo: 1 }
     ]);
 }
